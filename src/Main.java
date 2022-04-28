ffe82ce38669d997cdf4314799b8a94290486833
mashakulina <masha_kulina@mail.ru>
2022-04-28 14:55:42 +0300

    Мили

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 500;
+        int forMile = 20;
+
+        int Mile = amount / forMile;
+
+
+        System.out.println("Начислено миль: " + Mile);
+
+
+    }
+}
